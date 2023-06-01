e79c2cb84bbdf62fb178040ff4a08a15c036f342
jleous <jose-leonardo49@hotmail.com>
2023-06-01 09:26:14 -0400

    Nuevo

--- save_task.php
+++ save_task.php
@@ -1,6 +1,14 @@
 <?php
+include('db.php');
 
 if(isset($_POST['save_task'])){
-    echo $_POST['title'];
-    echo $_POST['description'];
+    $title= $_POST['title'];
+    $description= $_POST['description'];
+
+    $query = "INSERT INTO tasks (title, description) VALUES ('$title', '$description')";
+    $result = mysqli_query($conn,$query);
+    if(!$result){
+        die("Fallo la consulta");
+    }
+    header("Location: index.php");
 }
